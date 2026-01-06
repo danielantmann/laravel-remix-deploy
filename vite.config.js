28a26acf64cd15af48b57ca21bd73b74f5df18c6
Daniel Eric Antmann <tula3x3@gmail.com>
2026-01-06 15:39:57 +0100

    Fix asset URLs for production: force HTTPS base path for Vite on Render

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    base: "https://laravel-remix-deploy.onrender.com/",
     plugins: [
         laravel({
             input: ["resources/js/app.jsx"],
